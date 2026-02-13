d96f0378b78c41225de8cdbfa92ffd366d71a95a
Himanshu Choudhary <himanshuchoudhary9365@gmail.com>
2026-02-13 18:54:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const age = 18;
+
+const name = "Himanshu"
+
+function getName(name) {
+    return name2;
+}
+
+function getRoll() {
+    return "roll no";
+}
+
+function sale() {
+    return "sale";
+}
